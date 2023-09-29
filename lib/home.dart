55e4aa5abccdc804b0c42327a000b42e1e27f84a
rjayaa <rjaya.lelana03@gmail.com>
2023-09-29 22:30:07 +0700

    sign in and sign out finish

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,25 @@
+// import 'package:flutter/material.dart';
+
+// class MyHome extends StatefulWidget {
+//   const MyHome({super.key});
+
+//   @override
+//   State<MyHome> createState() => _MyHomeState();
+// }
+
+// class _MyHomeState extends State<MyHome> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Home"),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
@@ -8,11 +29,34 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Function to handle user logout
+  Future<void> _signOut() async {
+    try {
+      await _auth.signOut();
+      Navigator.of(context).pushReplacementNamed(
+          'phone'); // Navigate to phone verification page after logout
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          // Logout button
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () => _signOut(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("Welcome to the Home Page!"),
       ),
     );
   }
